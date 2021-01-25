f7e17d9f0757e0e5e01c62abae235238eea54a3a
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2021-01-25 18:35:15 +0200

    Tracked and resolved typo bug in application.properties

--- 02-backend/spring-boot-ecommerce/src/main/java/com/zah/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/zah/ecommerce/dao/ProductCategoryRepository.java
@@ -2,7 +2,9 @@ package com.zah.ecommerce.dao;
 
 import com.zah.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
