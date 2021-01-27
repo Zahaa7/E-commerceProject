85334213e5072f96ed2fd34978e8fa66e091960f
Alex Zaharioiu <alexzaharioiu7@gmail.com>
2021-01-27 13:39:37 +0200

    Added CrossOrigin support

--- 02-backend/spring-boot-ecommerce/src/main/java/com/zah/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/zah/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.zah.ecommerce.dao;
 
 import com.zah.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product, Long> {  // <Entity, PK>
 
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -13,7 +13,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.listProducts()
   }
 
